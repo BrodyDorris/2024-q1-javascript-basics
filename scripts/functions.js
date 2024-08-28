d60ae4232c6044bb463b07ab13f0f48df7df0460
Brody Dorris <Dorrisamanda@gmail.com>
2024-08-28 16:14:55 -0600

    Was Asked To Commit

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,18 +1,30 @@
 //@ts-check
 
+//can be called before defined since on the global scope
+add(1, 2)
+
 let firstName = "Brody";
 
-let sayHi = function (firstname, age) {
+//add default perameter values
+let sayHi = function (firstname = "Captain No Name", age = 100000) {
     let greeting = `Hello! my name is ${firstname} And I am ${age} years old.`;
     //"Hello! my name is "+ firstName + ". And I am "+ age +" years old.";
     console.log(greeting);
 };
+
+debugger;
+
+//call with default perameters
+sayHi();
+//call with default age
+sayHi("Jimmy");
 
 sayHi("Brody", 14);
 sayHi(firstName, 14);
 
 //console.log(firstName);
 
+// this fuction is hoisted to the global scope since it has no let= let sayHi() = function or fuction add()
 function add(a, b) {
     //return;
     let answer = a = b;
@@ -39,8 +51,7 @@ let person = {
 function WearBarneyShirt(person) {
 person.isWearingBarneyShirt = true;
 person.isTotalyAwesome = true;
-
-}
+};
 
 console.log(person);
 WearBarneyShirt(person);
