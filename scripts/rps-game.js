29392b7e75ef698f60d0665d686b5c618058bb85
Brody Dorris <Dorrisamanda@gmail.com>
2024-09-11 08:29:51 -0600

    Finshed Rps Game

--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -2,29 +2,68 @@
 
 let playerControlls = document.getElementById("player-controlls");
 
+//game-results
+/** @type {HTMLElement} */
+//@ts-ignore we know game results header is not null
+let gameResultsHeader = document.getElementById("game-results");
+
+gameResultsHeader.innerText = "Hey there!!";
+
+let isGameOver = false;
+
 let weapons = [
-    {
-        type: "Rock",
-        beats: "Scissors",
-    },
-    {
-        type:"Paper",
-        beats:"Rock"
-    },
-    {
-        type:"Scissors",
-        beats:"Paper"
-    }
+	{
+		type: "Rock",
+		beats: "Scissors",
+	},
+	{
+		type: "Paper",
+		beats: "Rock",
+	},
+	{
+		type: "Scissors",
+		beats: "Paper",
+	},
 ];
 
 function pickRandomWeopon(weapons) {
-    let randy = Math.floor(Math.random() * weapons.length);
-    return weapons[randy];
+	let randy = Math.floor(Math.random() * weapons.length);
+	return weapons[randy];
+}
+
+function determineOutcome(playerWeapon, computerWeapon) {
+	if (playerWeapon.type === computerWeapon.type) {
+		return "Its A Tie! Try Again!";
+	}
+
+	isGameOver = true;
+
+	if (playerWeapon.beats === computerWeapon.type) {
+		return `Player wins! ${playerWeapon.type} beats ${computerWeapon.type}`;
+	}
+
+    return `Computer wins! ${computerWeapon.type} beats ${playerWeapon.type}`;
 }
 
 function playerControllHandler(e) {
-    let weaponName = e.target.innerText;
-    console.log(weaponName, "was selected");
+	if (isGameOver) {
+		return;
+	}
+
+	let weaponName = e.target.innerText;
+	let playerWeapon = weapons.find((w) => w.type === weaponName);
+
+	if (!playerWeapon) {
+		console.log("Player wins! Choose your weapon Beats everything!");
+		return;
+	}
+
+	let computerWeapon = pickRandomWeopon(weapons);
+
+	let result = determineOutcome(playerWeapon, computerWeapon);
+    gameResultsHeader.innerText = result;
+
+	console.log(result);
 }
 
 playerControlls?.addEventListener("click", playerControllHandler);
