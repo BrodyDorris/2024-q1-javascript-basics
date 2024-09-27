e35b8cde429486aa4f71b075000062d2aef4252b
Brody Dorris <Dorrisamanda@gmail.com>
2024-09-27 11:09:35 -0600

    bouncing square

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -1,4 +1,5 @@
 //@ts-check
+import { SquareShape } from "./shapes/square.js";
 /** @type {HTMLCanvasElement} */
 //@ts-ignore canvas is an HTMLCanvasElement
 const canvas = document.getElementById("game-canvas");
@@ -7,8 +8,39 @@ const canvas = document.getElementById("game-canvas");
 //@ts-ignore ctx is an CanvasRenderingContext2D
 const ctx = canvas.getContext("2d");
 
-ctx.fillRect(0, 0, 50, 50);
+let s1 = new SquareShape(0, 0, ctx, canvas);
 
-ctx.beginPath();
-ctx.arc(100, 100, 25, 0,Math.PI * 2);
-ctx.fill();
+
+let shapes = [];
+
+for (let i = 0;i < 10000; i++) {
+    shapes.push(new SquareShape(0,0,ctx,canvas));
+}
+
+
+
+let lastTime = 0;
+
+let hue = 0;function drawLoop(timestamp) {
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    let elapsedTime = lastTime;
+    lastTime = timestamp;
+
+    s1.draw();
+    s1.update();
+
+    for (const shape of shapes) {
+        shape.update();
+            shape.draw();
+    }
+
+    //console.log(elapsedTime);
+
+
+
+    window.requestAnimationFrame(drawLoop);
+}
+
+
+window.requestAnimationFrame(drawLoop);
